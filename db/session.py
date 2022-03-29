9f40f9b610194fb3c28c4c81ea72e4e59162f888
Dmaina5054 <dmain5054@gmail.com>
2022-03-29 19:46:39 +0300

    depedency injection for functional tests

--- db/session.py
+++ db/session.py
@@ -1,6 +1,7 @@
 from django.template import engines
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 
 from core.config import settings
 
@@ -8,3 +9,13 @@ SQLALCHEMY_DATABASE_URL =   settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#use dependency injection
+#allow use test and prod db
+#during test override this and change db client
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db 
+    finally:
+        db.close()
